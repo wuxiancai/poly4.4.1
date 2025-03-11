a26e3ba315adb36dcd9972dbc0053bdf36a00447
xiancai <36890537+wuxiancai@users.noreply.github.com>
2025-03-11 12:57:52 +0800

    Add files via upload

--- xpath_config.py
+++ xpath_config.py
@@ -16,34 +16,34 @@ class XPathConfig:
         '//*[@id="authentication-modal"]/div/div[2]/div/div/div/div/div[3]/button[1]'
     ]
 
-    # 3.Buy按钮
+    # 3.Buy按钮长期有效
     BUY_BUTTON = [
-        '//button[(text()="Buy")]', # 长期有效
+        '//button[(text()="Buy")]', 
         '/html/body/div[1]/div[2]/div/div/div/div/div/div[1]/div[1]/div/div/button[1]'
     ]
 
-    # 4.Buy Yes 按钮
+    # 4.Buy Yes 按钮长期有效
     BUY_YES_BUTTON = [
-        '//button[.//span[contains(text(), "Yes")] and .//span[contains(text(), "¢")]]',# 长期有效
+        '//button[.//span[contains(text(), "Yes")] and .//span[contains(text(), "¢")]]',
         '/html/body/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div[1]/span[1]/button'
     ]
 
-    # 5.Buy No 按钮
+    # 5.Buy No 按钮长期有效
     BUY_NO_BUTTON = [
-        '//button[.//span[contains(text(), "No")] and .//span[contains(text(), "¢")]]',# 长期有效
+        '//button[.//span[contains(text(), "No")] and .//span[contains(text(), "¢")]]',
         '/html/body/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div[1]/span[2]/button'
     ]
 
-    # 6.Sell Yes 按钮
+    # 6.Sell Yes 按钮长期有效
     SELL_YES_BUTTON = [
-        '//button[.//span[contains(text(), "Yes")] and .//span[contains(text(), "¢")]]',# 长期有效
+        '//button[.//span[contains(text(), "Yes")] and .//span[contains(text(), "¢")]]',
         '/html/body/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div[1]/span[1]/button'
        
     ]
 
-    # 7.Sell No 按钮
+    # 7.Sell No 按钮长期有效
     SELL_NO_BUTTON = [
-        '//button[.//span[contains(text(), "No")] and .//span[contains(text(), "¢")]]'# 长期有效
+        '//button[.//span[contains(text(), "No")] and .//span[contains(text(), "¢")]]',
         
     ]
 
@@ -66,96 +66,96 @@ class XPathConfig:
         '//button[contains(text(), "Sell" and contains(text(), "Yes")) or contains(text(), "Sell" and contains(text(), "No"))]'
     ]
 
-    # 10.Amount输入框
+    # 10.Amount输入框长期有效
     AMOUNT_INPUT = [
         '//input[@id="market-order-amount-input"]',
-        '//p[text()="Amount"]/ancestor::div//input[@placeholder="$0"]',# 长期有效
+        '//p[text()="Amount"]/ancestor::div//input[@placeholder="$0"]',
         '/html/body/div[1]/div[2]/div/div/div/div/div/div[1]/div[3]/div/div/div/div/div[1]/div/div[1]/div[2]/div/div/input'
     ]
 
-    # 11.Position-Yes标签
+    # 11.Position-Yes标签长期有效
     POSITION_YES_LABEL = [
-        '//div[text()="Yes"]',# 长期有效
+        '//div[text()="Yes"]',
         '//div[@class="c-dhzjXW c-chKWaB c-chKWaB-eVTycx-color-green c-dhzjXW-ibxvuTL-css" and text()="Yes"]'
     ]
 
-    # 12.Position-No标签
+    # 12.Position-No标签长期有效
     POSITION_NO_LABEL = [
-        '//div[text()="No"]',# 长期有效
+        '//div[text()="No"]',
         '//div[@class="c-dhzjXW c-chKWaB c-chKWaB-kNNGp-color-red c-dhzjXW-ibxvuTL-css" and text()="No"]'
     ]
 
-    # 13.Position-Yes值
+    # 13.Position-Yes值长期有效
     POSITION_YES_VALUE = [
         '(//span[@class="c-PJLV c-dnafHo"])[1]',
         '/html/body/div[1]/div[2]/div/div/main/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/table/tbody/tr[1]/td[4]/span[2]',
         '//*[@id="event-detail-container"]/div/div[3]/div/div[2]/div/div[2]/table/tbody/tr[1]/td[4]/span[2]'
     ]
 
-    # 14.Position-No值
+    # 14.Position-No值长期有效
     POSITION_NO_VALUE = [
         '(//span[@class="c-PJLV c-dnafHo"])[2]',
         '/html/body/div[1]/div[2]/div/div/main/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/table/tbody/tr[2]/td[4]/span[2]'
     ]
 
-    # 15.Position-Sell按钮
+    # 15.Position-Sell按钮长期有效
     POSITION_SELL_BUTTON = [
-        '//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"]',# 长期有效
-        '//td//div//button[text()="Sell"]', # 长期有效
+        '//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"]',
+        '//td//div//button[text()="Sell"]', 
     ]
 
-    # 16.Position-Sell Yes按钮
+    # 16.Position-Sell Yes按钮 长期有效
     POSITION_SELL_YES_BUTTON = [
-        '(//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"])[1]',# 长期有效
+        '(//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"])[1]',
         '(//td//div//button[text()="Sell"])[1]'
         ]
 
-    # 17.Position-Sell No按钮
+    # 17.Position-Sell No按钮长期有效
     POSITION_SELL_NO_BUTTON = [
-        '(//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"])[2]',# 长期有效
+        '(//button[@class="c-gBrBnR c-gBrBnR-iifsICY-css"])[2]',
         '(//td//div//button[text()="Sell"])[2]'
         ]
 
-    # 18.Portfolio值
+    # 18.Portfolio值长期有效
     PORTFOLIO_VALUE = [
-        '(//span[@class="c-PJLV c-jaFKlk c-PJLV-ibdakYG-css"])[1]',# 长期有效
-        '//a[@href="/portfolio"]//span[contains(text(), "$")]' # 长期有效
+        '(//span[@class="c-PJLV c-jaFKlk c-PJLV-ibdakYG-css"])[1]',
+        '//a[@href="/portfolio"]//span[contains(text(), "$")]' 
     ]
 
-    # 19.Cash值
+    # 19.Cash值长期有效
     CASH_VALUE = [
-        '(//span[@class="c-PJLV c-jaFKlk c-PJLV-ibdakYG-css"])[2]',# 长期有效
-        '//button[.//span[text()="Cash"]]/span[contains(text(), "$")]', # 长期有效
+        '(//span[@class="c-PJLV c-jaFKlk c-PJLV-ibdakYG-css"])[2]',
+        '//button[.//span[text()="Cash"]]/span[contains(text(), "$")]', 
         '/html/body/div[1]/div[2]/nav[1]/div[1]/div[3]/div/nav/div/ul/div[1]/button[1]/span[1]'
     ]
 
-    # 20.History-交易记录
+    # 20.History-交易记录长期有效
     HISTORY = [
-        '(//div[@class="PJLV PJLV-ihovmxi-css"])[1]',# 长期有效
-        '(//div[@class="PJLV PJLV-ihovmxi-css"]//p)[1]', # 长期有效
+        '(//div[@class="PJLV PJLV-ihovmxi-css"])[1]',
+        '(//div[@class="PJLV PJLV-ihovmxi-css"]//p)[1]', 
         
         '/html/body/div[1]/div[2]/div/div/main/div/div/div/div/div/div[3]/div/div[5]/div/div[2]/div/p'  
     ]
     
-    # 21.搜索框
+    # 21.搜索框长期有效
     SEARCH_INPUT = [
-        '//input[@id="markets-grid-search-input"]',# 长期有效
+        '//input[@id="markets-grid-search-input"]',
         '/html/body/div[1]/div[2]/div/div/main/div/div/div[3]/div/div/div/div[1]/div[2]/div[2]/input',
         'input[placeholder="Search by market"]'
     ] 
 
-    # 22.Portfolio_button
+    # 22.Portfolio_button长期有效
     PORTFOLIO_BUTTON = [
-        '//a[@href="/portfolio"]' # 长期有效
+        '//a[@href="/portfolio"]' 
     ]
 
-    # 23.FIND_PORTFOLIO_COIN_BUTTON
+    # 23.FIND_PORTFOLIO_COIN_BUTTON长期有效
     FIND_PORTFOLIO_COIN_BUTTON = [
-        '//a[contains(text(), "above")]' # 长期有效
+        '//a[contains(text(), "above")]' 
     ]
 
-    # 24.search_confirm_button
+    # 24.search_confirm_button长期有效
     SEARCH_CONFIRM_BUTTON = [
-        '//p[contains(text(), "above")]', # 长期有效
-        '//a//p[contains(text(), "above")]' # 长期有效
+        '//p[contains(text(), "above")]', 
+        '//a//p[contains(text(), "above")]' 
     ]
